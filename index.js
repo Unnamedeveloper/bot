3bfae98a884262f3de2717b067c6aee409101154
Unnamedeveloper <unnamed123developer@gmail.com>
2024-11-05 22:47:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-const express = require("express");
-const app = express();
-
-app.listen(3000, () => {
-    console.log("Project is running!");
-});
-
-app.get("/", (req, res) => {
-    res.send("Hello world!");
-});
-
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
-client.on("messageCreate", message => {
-    if (message.content === "ping") {
-        message.channel.send("pong");
-    }
+client.once('ready', () => {
+  console.log('Discord bot is online!');
 });
 
-client.login(process.env.token);
+client.login(process.env.TOKEN);
