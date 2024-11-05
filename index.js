2fa2ad96c15cf13ce8949184dc4c189b41bad2bf
Unnamedeveloper <unnamed123developer@gmail.com>
2024-11-05 22:58:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
-
-client.once('ready', () => {
-  console.log('Discord bot is online!');
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
-client.login(process.env.token);
+client.once('ready', () => {
+  console.log('Bot is online!');
+});
+
+client.on('messageCreate', message => {
+  if (message.content === 'ping') {
+    message.channel.send('pong');
+  }
+});
+
+// Login using the token from the environment variable
+client.login(process.env.TOKEN);
